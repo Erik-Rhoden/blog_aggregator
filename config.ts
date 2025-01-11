00658e967bd2df4653e93693bdb8e897e45e34aa
Erik-Rhoden <erikrhoden@gmail.com>
2025-01-10 22:29:09 -0500

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,4 +1,37 @@
+import fs from "fs";
+import os from "os";
+import path from "path";
+
 type Config = {
     dbUrl: string;
-    currentUserName: string;
+    currentUserName?: string;
+}
+
+function readConfig(): Config {
+    try {
+        const file_path = getConfigFilePath()
+        const read = fs.readFileSync(file_path, 'utf8')
+        const rawConfig = JSON.parse(read)
+        const valid_check = validateConfig(rawConfig)
+        return valid_check
+    } catch (error) {
+        throw error;
+    }
+}
+
+function getConfigFilePath(): string {
+    return path.join(os.homedir(), ".gatorconfig.json");
+}
+
+function validateConfig(rawConfig: any): Config {
+    const config: Config = {} as Config
+    if ("db_url" in rawConfig && typeof rawConfig["db_url"] === "string") {
+        config["dbUrl"] = rawConfig["db_url"]
+    } else {
+        throw new Error("The url is invalid!")
+    }
+    if ("current_user_name" in rawConfig && typeof rawConfig["current_user_name"] === "string") {
+        config["currentUserName"] = rawConfig["current_user_name"]
+    }
+    return config
 }
